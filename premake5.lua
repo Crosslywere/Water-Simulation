38d74c9710cea3a50a9c1fee2d47ff7efc771c6b
Crosslywere <ogboruj@outlook.com>
2023-08-04 13:53:23 +0100

    Created project files

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "Water-Simulation"
+architecture "x64"
+configurations "Release"
+
+project "Water-Simulation"
+location "Water-Simulation"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++17"
+targetdir "bin/%{prj.name}"
+objdir "obj/%{prj.name}"
+files {"%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c", "%{prj.name}/src/**.h"}
+includedirs {"Dependencies/Glad/include"}
+
+optimize "On"
+
+filter "system:windows"
+systemversion "latest"
+links {"opengl32.lib"}
