8d2746a50ccd3610635b368e5f09240b7a8b60b3
Crosslywere <ogboruj@outlook.com>
2023-08-04 14:38:53 +0100

    Added GLFW to project

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,12 @@
+-- Workspace
 workspace "Water-Simulation"
 architecture "x64"
 configurations "Release"
 
+-- Include projects
+include "Dependencies/GLFW"
+
+-- Main project
 project "Water-Simulation"
 location "Water-Simulation"
 kind "ConsoleApp"
@@ -10,10 +15,13 @@ cppdialect "C++17"
 targetdir "bin/%{prj.name}"
 objdir "obj/%{prj.name}"
 files {"%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c", "%{prj.name}/src/**.h"}
-includedirs {"Dependencies/Glad/include"}
+includedirs {"Dependencies/Glad/include", "Dependencies/GLFW/include"}
+links {"GLFW"}
 
+-- Because only building release optimize is on
 optimize "On"
 
+-- For windows systems
 filter "system:windows"
 systemversion "latest"
 links {"opengl32.lib"}
