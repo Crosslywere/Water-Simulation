8f6ecc7145910e23796b17b20c1dbf9157d3a8d5
Crosslywere <ogboruj@outlook.com>
2023-08-04 14:39:34 +0100

    Test implementation

--- Water-Simulation/src/main.cpp
+++ Water-Simulation/src/main.cpp
@@ -1,7 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(void)
 {
-    std::cout << "Hello, Water!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(1600, 900, "Water Simulation", nullptr, nullptr);
+    if (!window) return -1;
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+    std::cout << "Hello, Water Sim!" << std::endl;
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
