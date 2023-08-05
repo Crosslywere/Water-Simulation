43aa73c935c7c11ca484bf4b26a01008a8b97907
Crosslywere <ogboruj@outlook.com>
2023-08-05 09:16:08 +0100

    Created setter functions

--- Water-Simulation/src/Shader.h
+++ Water-Simulation/src/Shader.h
@@ -8,7 +8,9 @@
 #include <fstream>
 #include <iostream>
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+// Enumeration for the 3 types of shaders
 enum class ShaderType
 {
 	None = 0,
@@ -17,12 +19,14 @@ enum class ShaderType
 	Fragment = GL_FRAGMENT_SHADER
 };
 
+// Structure that points to a shader file via filePath and holds what type of shader the file is
 struct ShaderFile
 {
 	std::string filePath = "";
 	ShaderType type = ShaderType::None;
 };
 
+// Class used to interact with shaders
 class Shader
 {
 public:
@@ -31,13 +35,34 @@ public:
 		std::vector<unsigned int> shaders;
 		if (shaderFile1.type != ShaderType::None)
 		{
-			shaders.push_back(CompileShader(shaderFile1.filePath, static_cast<unsigned int>(shaderFile1.type)));
+			try
+			{
+				shaders.push_back(CompileShader(shaderFile1.filePath, static_cast<unsigned int>(shaderFile1.type)));
+			}
+			catch (std::exception e)
+			{
+				std::cout << e.what() << std::endl;
+			}
 			if (shaderFile2.type != ShaderType::None)
 			{
-				shaders.push_back(CompileShader(shaderFile2.filePath, static_cast<unsigned int>(shaderFile2.type)));
+				try
+				{
+					shaders.push_back(CompileShader(shaderFile2.filePath, static_cast<unsigned int>(shaderFile2.type)));
+				}
+				catch (std::exception e)
+				{
+					std::cout << e.what() << std::endl;
+				}
 				if (shaderFile3.type != ShaderType::None)
 				{
-					shaders.push_back(CompileShader(shaderFile3.filePath, static_cast<unsigned int>(shaderFile3.type)));
+					try
+					{
+						shaders.push_back(CompileShader(shaderFile3.filePath, static_cast<unsigned int>(shaderFile3.type)));
+					}
+					catch (std::exception e)
+					{
+						std::cout << e.what() << std::endl;
+					}
 				}
 			}
 			m_ProgramID = glCreateProgram();
@@ -48,18 +73,46 @@ public:
 		}
 		for (auto shader : shaders)
 			glDeleteShader(shader);
-
 	}
+	
 	~Shader()
 	{
 		if (m_ProgramID != 0)
 			glDeleteProgram(m_ProgramID);
 	}
+
+	// Switch shader to shader instance.
 	void Use()
 	{
 		glUseProgram(m_ProgramID);
 	}
+	
+	// Disables all shaders until Use is called from a shader instance
+	static void Disable()
+	{
+		glUseProgram(0);
+	}
+
+	// Sets a float uniform with a matching name in the file to be the value of f
+	void SetFloat(const std::string& name, float f)
+	{
+		glUniform1f(glGetUniformLocation(m_ProgramID, name.c_str()), f);
+	}
+
+	// Sets a vec3 uniform with a matching name in the file to be the value of v
+	void SetVec3(const std::string& name, glm::vec3 v)
+	{
+		glUniform3fv(glGetUniformLocation(m_ProgramID, name.c_str()), GL_FALSE, glm::value_ptr(v));
+	}
+
+	// Sets a mat4 uniform with a matching name to the file to be the value of m
+	void SetMat4(const std::string& name, glm::mat4 m)
+	{
+		glUniformMatrix4fv(glGetUniformLocation(m_ProgramID, name.c_str()), 1, GL_FALSE, glm::value_ptr(m));
+	}
+
 private:
+	// Reads in the shader from the file path compiles the shader
 	static unsigned int CompileShader(const std::string& shaderPath, unsigned int type)
 	{
 		std::ifstream sourceFile(shaderPath);
@@ -78,8 +131,9 @@ private:
 		}
 		else
 		{
-			std::cerr << "\033[31mUnable to open shader file " << shaderPath << "\033[0m" << std::endl;
+			// std::cerr << "\033[31mUnable to open shader file " << shaderPath << "\033[0m" << std::endl;
 			sourceFile.close();
+			throw std::exception(std::string("Unable to open shader file in path '" + shaderPath).c_str());
 			return 0;
 		}
 	}
