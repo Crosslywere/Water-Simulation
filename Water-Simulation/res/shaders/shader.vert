71bcbe2607b1174f2e4b50d04de8c13c6d2dc729
Crosslywere <ogboruj@outlook.com>
2023-08-07 07:27:23 +0100

    Implemented a simple sine wave offset in vertex shader

--- Water-Simulation/res/shaders/shader.vert
+++ Water-Simulation/res/shaders/shader.vert
@@ -1,12 +1,26 @@
 #version 330 core
 
+#define FREQUENCY(wavelength) 2.0 / (wavelength)
+
 layout (location = 0) in vec3 aPos;
 
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
+uniform float time;
+
 void main()
 {
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
+	float posOffset = aPos.x + aPos.z;
+
+	float freq = FREQUENCY(1.0);
+
+	// The offset for the y-axis calculated for each vertex
+	float yOffset = 0.25 * sin((aPos.y * freq + time * freq) + posOffset);
+
+	// The new vertex position after the offset has been calculated
+	vec3 newPos = vec3(aPos.x, yOffset, aPos.z);
+
+	gl_Position = projection * view * model * vec4(newPos, 1.0);
 }
--- Water-Simulation/src/main.cpp
+++ Water-Simulation/src/main.cpp
@@ -18,6 +18,22 @@ void frameBufferResizeFn(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void keyFn(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    switch (key)
+    {
+    case GLFW_KEY_1:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        break;
+    case GLFW_KEY_2:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        break;
+    case GLFW_KEY_3:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+        break;
+    }
+}
+
 int main(void)
 {
     // Initializing GLFW
@@ -27,7 +43,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // Configuring the window
-    WindowProps props = { 1600, 900, "Water Simulation" };
+    WindowProps props = { 1280, 720, "Water Simulation", nullptr };
     // Creating the Window
     GLFWwindow* window = glfwCreateWindow(props.Width, props.Height, props.Title.c_str(), nullptr, nullptr);
     // Testing that the Window was created
@@ -36,27 +52,31 @@ int main(void)
     glfwSetWindowSizeLimits(window, 800, 600, GLFW_DONT_CARE, GLFW_DONT_CARE);
     glfwSetWindowUserPointer(window, &props);
     glfwSetFramebufferSizeCallback(window, frameBufferResizeFn);
+    glfwSetKeyCallback(window, keyFn);
     // Setting the Window to be the current render context
     glfwMakeContextCurrent(window);
     // Loading OpenGL functions via Glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
     std::cout << "OpenGL Version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    glfwSwapInterval(1);
     // Loading shader resource
     Shader shader({ "res/shaders/shader.frag", ShaderType::Fragment }, { "res/shaders/shader.vert", ShaderType::Vertex });
-    Mesh mesh("res/meshes/test.obj");
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    Mesh mesh("res/meshes/plane.obj");
     // The application loop
     while (!glfwWindowShouldClose(window))
     {
         // Clearing the colors of the display
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // TODO: Draw mesh using shader
+        // Setting the shaders uniforms
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)props.Width / (float)props.Height, 0.1f, 100.0f);
         shader.SetMat4("projection", projection);
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
-        shader.SetMat4("model", model);
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, -3.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 3.0f, -13.0f), glm::vec3(0.0f), glm::normalize(glm::vec3(0.0f, 1.0f, 1.0f)));
         shader.SetMat4("view", view);
+        shader.SetMat4("model", glm::rotate(glm::mat4(1.0f), glm::radians(-15.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+        shader.SetFloat("time", (float)glfwGetTime());
+        shader.SetVec3("right", glm::vec3(1.0f, 0.0f, 0.0f));
+        shader.SetVec3("lightDir", glm::normalize(glm::vec3(-1.0f, -1.0f, 0.0f)));
+        // Drawing the mesh
         mesh.Draw(shader);
         // Swapping the buffers
         glfwSwapBuffers(window);
