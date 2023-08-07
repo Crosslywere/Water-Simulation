43bd3ce2453956377c7e8836f5b67837aa9b911b
Crosslywere <ogboruj@outlook.com>
2023-08-07 22:33:10 +0100

    Implemented camera and camera controls

--- Water-Simulation/src/main.cpp
+++ Water-Simulation/src/main.cpp
@@ -2,12 +2,17 @@
 #include <GLFW/glfw3.h>
 #include "Shader.h"
 #include "Mesh.h"
+#include "Camera.h"
 #include <iostream>
 
 struct WindowProps
 {
+    // The Windows width and height
     int Width, Height;
+    // The Windows title
     std::string Title;
+    // The Camera (stored as a pointer to optimize for space)
+    Camera* ptrCamera;
 };
 
 void frameBufferResizeFn(GLFWwindow* window, int width, int height)
@@ -23,15 +28,85 @@ void keyFn(GLFWwindow* window, int key, int scancode, int action, int mods)
     switch (key)
     {
     case GLFW_KEY_1:
+    case GLFW_KEY_F1:
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         break;
     case GLFW_KEY_2:
+    case GLFW_KEY_F2:
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         break;
     case GLFW_KEY_3:
+    case GLFW_KEY_F3:
         glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
         break;
+    case GLFW_KEY_ESCAPE:
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        break;
     }
+}
+
+void processInput(GLFWwindow* window, Camera& camera, float delta)
+{
+    static float speed = 2.5f;
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        speed = 5.0f;
+    else
+        speed = 2.5f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.Move(camera.GetFront() * speed * delta);
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.Move(-camera.GetFront() * speed * delta);
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.Move(-camera.GetRight() * speed * delta);
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.Move(camera.GetRight() * speed * delta);
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.Move(Camera::WorldUp * speed * delta);
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+        camera.Move(-Camera::WorldUp * speed * delta);
+}
+
+void cursorPosFn(GLFWwindow* window, double xpos, double ypos)
+{
+    static bool firstMouse = true;
+    Camera& camera = *((WindowProps*)glfwGetWindowUserPointer(window))->ptrCamera;
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        static float xlast = 0.0f;
+        static float ylast = 0.0f;
+        static float past = 0.0f;
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        if (firstMouse)
+        {
+            xlast = static_cast<float>(xpos);
+            ylast = static_cast<float>(ypos);
+            firstMouse = false;
+        }
+        float xoffset = static_cast<float>(xpos) - xlast;
+        float yoffset = ylast - static_cast<float>(ypos);
+        camera.PitchBy(yoffset * 0.1f);
+        camera.YawBy(xoffset * 0.1f);
+        xlast = static_cast<float>(xpos);
+        ylast = static_cast<float>(ypos);
+    }
+    else
+    {
+        firstMouse = true;
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
+}
+
+void scrollFn(GLFWwindow* window, double xoffset, double yoffset)
+{
+    Camera& camera = *((WindowProps*)glfwGetWindowUserPointer(window))->ptrCamera;
+    camera.DecFOV(yoffset);
 }
 
 int main(void)
@@ -43,7 +118,8 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // Configuring the window
-    WindowProps props = { 1280, 720, "Water Simulation" };
+    WindowProps props = { 1280, 720, "Water Simulation", new Camera(glm::vec3(3.0f, 6.0f, 10.0f), -30.0f, -105.0f, 45.0f) };
+    Camera& camera = *props.ptrCamera;
     // Creating the Window
     GLFWwindow* window = glfwCreateWindow(props.Width, props.Height, props.Title.c_str(), nullptr, nullptr);
     // Testing that the Window was created
@@ -53,6 +129,8 @@ int main(void)
     glfwSetWindowUserPointer(window, &props);
     glfwSetFramebufferSizeCallback(window, frameBufferResizeFn);
     glfwSetKeyCallback(window, keyFn);
+    glfwSetCursorPosCallback(window, cursorPosFn);
+    glfwSetScrollCallback(window, scrollFn);
     // Setting the Window to be the current render context
     glfwMakeContextCurrent(window);
     // Loading OpenGL functions via Glad
@@ -63,14 +141,20 @@ int main(void)
     Shader shader({ "res/shaders/shader.frag", ShaderType::Fragment }, { "res/shaders/shader.vert", ShaderType::Vertex });
     Mesh mesh("res/meshes/plane.obj");
     // The application loop
+    float past = static_cast<float>(glfwGetTime());
     while (!glfwWindowShouldClose(window))
     {
+        // Calculating the time delta
+        float now = static_cast<float>(glfwGetTime());
+        float delta = now - past;
+        past = now;
         // Clearing the colors of the display
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        processInput(window, camera, delta);
         // Setting the shaders uniforms
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)props.Width / (float)props.Height, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.GetFOV()), (float)props.Width / (float)props.Height, 0.1f, 100.0f);
         shader.SetMat4("projection", projection);
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 6.0f, -10.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 view = camera.GetViewMatrix();
         shader.SetMat4("view", view);
         glm::mat4 model = glm::mat4(1.0f);
         shader.SetMat4("model", model);
