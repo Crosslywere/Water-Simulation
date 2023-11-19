7a1d3659ff9d7dd2458b73e8d48dec5e4f0297a5
Crosslywere <ogboruj@outlook.com>
2023-11-19 13:17:49 +0100

    Added postbuildcommand

--- premake5.lua
+++ premake5.lua
@@ -17,6 +17,7 @@ objdir "obj/%{prj.name}"
 files {"%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c", "%{prj.name}/src/**.h"}
 includedirs {"Dependencies/Glad/include", "Dependencies/GLFW/include", "Dependencies/GLM/include", "Dependencies/stb"}
 links {"GLFW"}
+postbuildcommands { "{COPYDIR} res/ ../bin/%{prj.name}/res/" }
 
 -- Because only building release optimize is on
 optimize "On"
